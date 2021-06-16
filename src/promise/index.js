3512f1512979a5049bbbd7d37d7712580d3a7c7f
Julian Henao Henao <jhenaohenao@gmail.com>
2021-06-16 13:10:47 -0500

    Multiple Request to the Rick and Morty API with Promises

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,6 +1,6 @@
 const somethingWillHappen = () => {
   return new Promise((resolve, reject) => {
-    if (false) {
+    if (true) {
       resolve("Hey!");
     } else {
       reject("Whooops!");
@@ -9,5 +9,32 @@ const somethingWillHappen = () => {
 };
 
 somethingWillHappen()
-.then(response => console.log(response))
-.catch(err => console.error(err));
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+somethingWillHappen_2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve("True");
+      }, 2000);
+    } else {
+      const error = new Error("Whooops!");
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen_2()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+// Promesas encadenadas
+
+Promise.all([somethingWillHappen(), somethingWillHappen_2()])
+  .then((response) => {
+    console.log("Array of results ", response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
